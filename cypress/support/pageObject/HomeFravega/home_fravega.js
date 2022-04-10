783f2e5c17073f88e4fe34b19c53606e08d3103d
Fernando  Morales <fersebamorales@gmail.com>
2022-04-09 22:48:57 -0300

    se corrigen metodos en la clase home_fravega

--- cypress/support/pageObject/HomeFravega/home_fravega.js
+++ cypress/support/pageObject/HomeFravega/home_fravega.js
@@ -1,73 +1,72 @@
 require("cypress-xpath");
 class Home_Fravega {
-
   visitarSitio() {
-
-    let tiempo = 2000
-    
+    let tiempo = 2000;
 
     before(() => {
       cy.visit("https://www.fravega.com/");
       cy.title("eq","Frávega: Electrodomésticos, Tecnología y Artículos para el hogar")
       cy.wait(tiempo)
-      cy.fixture('datosFravega').then((data) => {globalThis.data = data})
+      cy.fixture("datosFravega").then((data) => {
+        globalThis.data = data;
+      })
     })
   }
 
   buscarProducto(producto) {
-    let tiempo = 2000
+    let tiempo = 2000;
 
     //Cerrar Pop up de codigo Postal
-    cy.xpath("//button[@data-test-id='close-modal-button']").click()
+    cy.xpath("//button[@data-test-id='close-modal-button']").click();
     cy.wait(tiempo)
     //Validamos caja de busqueda e Ingresamos el producto a buscar.Presionamos Enter
-    cy.get('[style="grid-area:search"] > .sc-higWrZ > .sc-gTgzoy').should("be.visible").then(() => {
-      cy.get('[style="grid-area:search"] > .sc-higWrZ > .sc-gTgzoy').type(producto).type("{enter}")
-       cy.wait(tiempo)
-    })
-    //Validar titulo de la seccion del producto seleccionado
-    cy.xpath("//h1[contains(text(),'Heladera')]").should("contains.text",producto)
+    cy.get('[style="grid-area:search"] > .sc-higWrZ > .sc-gTgzoy').should("be.visible")
+    .then(() => {
+        cy.get('[style="grid-area:search"] > .sc-higWrZ > .sc-gTgzoy').type(producto).type("{enter}")
+        cy.wait(tiempo)
+        //Validar titulo de la seccion del producto seleccionado
+        cy.xpath("//h1[contains(text(),'Heladera')]").should("contains.text",producto)
+      })
   }
 
-  //ACA QUEDE
-  /* seleccionarTipoProducto(tipoProducto, busqueda) {
-
+  seleccionarTipoProducto(tipoProducto, busqueda) {
     let tiempo = 2000
-    let enlace 
 
     //listado de tipos de productos
-    cy.xpath("//body[1]/div[1]/div[2]/div[3]/div[4]").each(($el) => {
-         cy.log("Listado: " + $el)
-         for(let enlace in $el.text()){
-           if(enlace.includes(tipoProducto)){
-            cy.wrap(enlace).click()
-            cy.log("Parametro enviado: " + tipoProducto)
-            cy.log("Hizo click en:" +enlace)
-            cy.get('.categorySlug__TitleCategory-shopping-ui__sc-1l2p1q1-6').should("contains.text", busqueda) 
-            break
-          }
-       }   
-   })
-}
-       
-
-  seleccionarMarca(marcaSeleccionada, titulo) {
-
-    let tiempo = 2000
-    //Seleccionar la opcion ver todos
-    cy.get(".BrandFilter__Hyperlink-shopping-ui__sc-1f2zp6r-0").click()
-    //Filtrar por marca seleccionada
-    cy.get(".styled__SearchInput-shopping-ui__sc-pa3x91-2").type(marcaSeleccionada)
-    //Clickear sobre el enlace
-    cy.get("#filterItempatrick").click()
-    cy.get("#apply").click()
-    //Validar el titulo de la seccion = Producto + marca
-    cy.get(".categorySlug__TitleCategory-shopping-ui__sc-1l2p1q1-6").should("have.text",titulo)
-    cy.wait(tiempo)
-
+    cy.xpath("//div[@class='sc-ad9dc01-1 iakjIl']/div/ul/li/h4/a").each(($el) => {
+        cy.log("enlace: " + $el.text())
+        if ($el.text().includes(tipoProducto)) {
+          cy.wrap($el).click()
+          cy.log("Hizo click en : " + $el.text())
+          return false
+        }
+      }).then(() => {
+        cy.get(".sc-ad9dc01-6").should("contains.text", busqueda)
+      })
   }
 
-  validarMarcaPorProductoyCantidad(marca){
+  seleccionarMarca(marcaSeleccionada, titulo) {
+    let tiempo = 2000
+
+    //Seleccionar la opcion ver todos
+    cy.get(".sc-8de229fa-0").click()
+    //Filtrar por marca seleccionada 
+    cy.xpath("//div[@class='sc-c0f66744-4 drldtH']/label").each(($el) => {
+      cy.log("enlace: " + $el.text())
+      if($el.text().includes(marcaSeleccionada)){
+        cy.wrap($el).click()
+        cy.log("Hizo click en : " + $el.text())
+        return false
+      }
+   }).then(() => {
+     cy.get("#apply").should("be.visible").click()
+     //Validar el titulo de la seccion = Producto + marca
+    cy.get(".sc-ad9dc01-6").should("have.text",titulo)
+    cy.wait(tiempo)
+   })
+}
+
+  /*validarMarcaPorProductoyCantidad(marca){
 
       cy.xpath("//ul[@data-test-id='results-list']/li/article/a/div/div/span").each(($el,$index,$list) =>{   
            cy.log("Producto: " + $el.text())
@@ -76,9 +75,5 @@ class Home_Fravega {
       })
    }  */
 }
-//cy.log("Producto: " + $el.text())
-//cy.get($el).should("contains.text",marca)  
-//cy.get("//span[contains(text(),'Siguiente >')]").should("be.visible")
 
-
-export default Home_Fravega
+export default Home_Fravega;
